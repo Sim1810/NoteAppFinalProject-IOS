b0b0e3759c3135164ea50cfef89fa3b0af36ada1
Sim1810 <kaursim1810@gmail.com>
2020-06-18 20:36:51 -0400

    function getAllNoteBooks

---
+++ NoteAppFinalProject IOS/EditNotesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EditNotesViewController.swift
+//  NoteAppFinalProject IOS
+//
+//  Created by User on 6/18/20.
+//  Copyright © 2020 Simranjeet kaur. All rights reserved.
+//
+
+import UIKit
+
+class EditNotesViewController: UIViewController {
+    
+    var note:Note!
+    var notebook : Notebook?
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
