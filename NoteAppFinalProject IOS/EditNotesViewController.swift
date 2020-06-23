3abcc268a864654ea1f55f114f5d5c30b0d4c01d
Kirandeep Kaur <kiranlongia912@gmail.com>
2020-06-23 11:17:14 -0400

    SAVE Button in editTVC

--- NoteAppFinalProject IOS/EditNotesViewController.swift
+++ NoteAppFinalProject IOS/EditNotesViewController.swift
@@ -37,16 +37,17 @@ class EditNotesViewController: UIViewController,  UINavigationControllerDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        txtView.layer.borderWidth = 1
+        txtView.layer.borderColor = UIColor.black.cgColor
         
-         let appDelegate = UIApplication.shared.delegate as? AppDelegate
-           
-        context = appDelegate?.persistentContainer.viewContext
+        
+         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+              context = appDelegate.persistentContainer.viewContext
         if (userIsEditing == true) {
             print("Editing an existing note")
-          //  titleLbl.text = note.title!
-            //txtView.text = note.text!
-          //  self.showImg.image = UIImage(data: note.image! as Data)
+           // titleLbl.text = note.title!
+           // txtView.text = note.text!
+           // self.showImg.image = UIImage(data: note.image! as Data)
            
             mapbtn.isHidden = false
         }
@@ -101,6 +102,62 @@ class EditNotesViewController: UIViewController,  UINavigationControllerDelegate
        }
     
     
+    
+    @IBAction func saveBtn(_ sender: UIBarButtonItem) {
+       // determineMyCurrentLocation()
+                     if (txtView.text!.isEmpty) {
+                         print("Please enter some text")
+                         return
+                     }
+                     
+                     
+                     if (userIsEditing == true) {
+                        // note.text = txtView.text!
+                     }
+                     else {
+                         
+                         // create a new note in the notebook
+                         self.note = Note(context:context)
+                         note.setValue(Date(), forKey:"dateAdded")
+                         if (titleLbl.text!.isEmpty) {
+                             note.title = "No Title"
+                         }
+                         else{
+                           //  note.title = titleLbl.text!
+                         }
+                         note.text = txtView.text!
+                      let imageData = showImg.image!.pngData() as NSData?
+                         note.image = imageData as Data?
+                        
+                         note.notebook = self.notebook
+                     }
+                     
+                     do {
+                         try context.save()
+                         print("Note Saved!")
+                         
+                         
+                         // show an alert box
+                         let alertBox = UIAlertController(title: "Saved!", message: "Save Successful.", preferredStyle: .alert)
+                         alertBox.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                         self.present(alertBox, animated: true, completion: nil)
+                     }
+                     catch {
+                         print("Error saving note in Edit Note screen")
+                         
+                         // show an alert box with an error message
+                         let alertBox = UIAlertController(title: "Error", message: "Error while saving.", preferredStyle: .alert)
+                         alertBox.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                         self.present(alertBox, animated: true, completion: nil)
+                     }
+                     
+                     if (userIsEditing == false) {
+                         self.navigationController?.popViewController(animated: true)
+                         //self.dismiss(animated: true, completion: nil)
+                     }
+                     
+                     
+          }
     
     
 
